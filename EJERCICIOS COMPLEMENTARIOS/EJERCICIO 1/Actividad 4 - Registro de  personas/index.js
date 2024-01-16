5c96e3ed91344cce7366fd85614e5adf71234f1e
Federico Gabriel Gutierrez <gutierrezfedericog@gmail.com>
2024-01-16 02:17:52 -0300

    Actualización

---
+++ EJERCICIOS COMPLEMENTARIOS/EJERCICIO 1/Actividad 4 - Registro de  personas/index.js
@@ -0,0 +1,8 @@
+let familiarUno = prompt("Ingresa el nombre de un integrante de tu familia");
+let familiarDos = prompt("Ingresa el nombre de un integrante de tu familia");
+let familiarTres = prompt("Ingresa el nombre de un integrante de tu familia");
+let familiarCuarto = prompt("Ingresa el nombre de un integrante de tu familia");
+let familiarQuinto = prompt("Ingresa el nombre de un integrante de tu familia");
+
+console.log("Los integrantes de tu familia son: " + familiarUno + ", " + familiarDos + ", " + familiarTres +", "  + familiarCuarto + " y " + familiarQuinto);
+alert("Los integrantes de tu familia son: " + familiarUno + ", " + familiarDos + ", " + familiarTres +", "  + familiarCuarto + " y " + familiarQuinto);
