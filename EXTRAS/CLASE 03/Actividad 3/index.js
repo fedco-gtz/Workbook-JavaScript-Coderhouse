f34b4ce1b47e01cdbcd812164c1f90d3713992e2
Federico Gabriel Gutierrez <gutierrezfedericog@gmail.com>
2024-01-18 14:42:09 -0300

    Actualización

--- EXTRAS/CLASE 03/Actividad 3/index.js
+++ EXTRAS/CLASE 03/Actividad 3/index.js
@@ -1,40 +1,39 @@
-let numeroUno = parseFloat(prompt("Ingrese el primer número"));
-let numeroDos = parseFloat(prompt("Ingrese el segundo número"));
-let operacion = prompt("Seleccione la operación a realizar (+, -, x, /)");
+let continuar = "SI";
 let resultado;
-let continuar;
 
 do {
-    if (!isNaN(numeroUno) && !isNaN(numeroUno)) {
+    let numeroUno = parseFloat(prompt("Ingrese el primer número"));
+    let numeroDos = parseFloat(prompt("Ingrese el segundo número"));
+    let operacion = prompt("Seleccione la operación a realizar (+, -, x, /)");
+
+    if (!isNaN(numeroUno) && !isNaN(numeroDos)) {
         if (operacion === "+") {
             resultado = numeroUno + numeroDos;
-            continuar = "NO"
+          console.log(numeroUno + " + " + numeroDos + " = " + resultado);
         } else if (operacion === "-") {
             resultado = numeroUno - numeroDos;
-            continuar = "NO"
+            console.log(numeroUno + " - " + numeroDos + " = " + resultado);
         } else if (operacion === "x") {
             resultado = numeroUno * numeroDos;
-            continuar = "NO"
+            console.log(numeroUno + " x " + numeroDos + " = " + resultado);
         } else if (operacion === "/") {
             if (numeroDos !== 0) {
                 resultado = numeroUno / numeroDos;
-                continuar = "NO"
+                console.log(numeroUno + " / " + numeroDos + " = " + resultado);
             } else {
                 console.log("Error: No se puede dividir por cero.");
-                continuar = "NO"
+              break;
             }
         } else {
-            console.log("Ingresa una operacion valida");
-            continuar = "NO";
+            console.log("Ingresa una operación válida");
+          break;
         }
     } else {
-        console.log("Ingresa un numero valido");
-        continuar = "NO";
+        console.log("Ingresa números válidos");
+      break;
     }
 
-} while (continuar !== "NO");
+    continuar = prompt("¿Desea continuar? (SI/NO)").toUpperCase();
+} while (continuar === "SI");
 
-
-console.log("Resultado: " + resultado);
-
-// CODIGO AVANZADO, VERIFICAR MAS LO DE EL CONTINUAR Y VER SI SE PUEDE ARREGLAR ALGO MAS
+console.log("Hasta luego");
